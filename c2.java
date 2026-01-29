6cd56645b35c7cf3615a9c5fdc861899a1b97dfa
uzxcontato-ui <uzxcontato@gmail.com>
2026-01-29 19:04:11 -0300

    primeiro commit via vscode

---
+++ c2.java
@@ -0,0 +1,24 @@
+//Padroes de desenvolvimento e conceitos
+
+import java.util.*;
+
+public class c2 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in); 
+        /*
+        *conceito de package *criando direto com classe ou apenas *invertendo o exemplo iv.br.*com --> com.br.iv 
+        */
+        System.out.println("Ola, informe seu nome");
+        String name = scanner.next();
+        System.out.println("Ola, informe seu nome");
+        int age = scanner.nextln("Ola %s sua idade e %s"+name +age);
+        /*println --> usa quebra de linha 
+        * print --> faz impressao toda junta 
+        */
+
+
+
+
+    }
+}
