177e915702ff6dd00eb116e47bf7b31f713ee1a7
uzxcontato-ui <uzxcontato@gmail.com>
2026-02-10 07:34:41 -0300

    add operadores

--- c6.java
+++ c6.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
-//bitwise(bit a bit )-tipo long
+// bitwise (bit a bit) - tipo long
 
 public class c6 {
 
-    public static void main(String[] args){
-    var value1 = 6;
-    var binary = Integer.toBinaryString(value1);    
-    System.out.printf("Primeiro numero da operaçao %s (representacao binaria %S)\n", value1, binary1);
-    var value2 = 12;
-    var binary2 = Integer.toBinaryString(value2);  
-    System.out.printf("Primeiro numero da operaçao %s (representacao binaria %S)\n", value2, binary2);
-    var result =  value1 | value2;
-    var binaryResult = Integer.toBinaryString(result);
-    System.out.printf("%s| %s = %s\n", value1, value2, value1 | value2, binaryResult);
-    System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+    public static void main(String[] args) {
+        var value1 = 6;
+        var binary1 = Integer.toBinaryString(value1);    
+        System.out.printf("Primeiro numero da operação %s (representação binária %s)\n", value1, binary1);
 
+        var value2 = 12;
+        var binary2 = Integer.toBinaryString(value2);  
+        System.out.printf("Segundo numero da operação %s (representação binária %s)\n", value2, binary2);
+
+        var result = value1 | value2;
+        var binaryResult = Integer.toBinaryString(result);
+        System.out.printf("%s | %s = %s (binário: %s)\n", value1, value2, result, binaryResult);
+
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
     }
     
     /*
-    
-    
     
     */
 }
--- coperadores.java
+++ coperadores.java
@@ -1,25 +1,33 @@
 import java.util.Scanner;
 
 public class coperadores {
-    public class void main(String[] args){
-    var scanner = new Scanner(System.in);
-    System.out.println("Informe o primeiro numero: ");
-    var value1 = scanner.nextInt();
-    System.out.printf("a raiz quadradda de %s e %s\n",value1 ,Math.pow(value1,2));
-    //System.out.println("Informe o primeiro numero: ");
-    //var value2 = scanner.nextInt();
-    System.out.printf(value1 +"+"+value2 + "="+ value1 + value2);
-    // System.out.printf("%s + %s = %s \n", value1, value2, value1 + value2);
-    // System.out.printf("%s / %s = %s \n", value1, value2, value1 / value2);aqui temos que trabalhr com float em alguns casos 
-    // System.out.printf("%s %% %s = %s \n", value1, value2, value1 % value2);
-    // System.out.printf("%s * %s = %s \n", value1, value2, value1 * value2);
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro numero: ");
+        var value1 = scanner.nextInt();
+
+        System.out.printf("o quadrado de %s é %s\n", value1, Math.pow(value1, 2));
+
+        // System.out.println("Informe o segundo numero: ");
+        // var value2 = scanner.nextInt();
+
+        // System.out.printf(value1 +"+"+value2 + "="+ (value1 + value2));
+        // System.out.printf("%s + %s = %s \n", value1, value2, value1 + value2);
+        // System.out.printf("%s / %s = %s \n", value1, value2, (float) value1 / value2); // aqui temos que trabalhar com float em alguns casos 
+        // System.out.printf("%s %% %s = %s \n", value1, value2, value1 % value2);
+        // System.out.printf("%s * %s = %s \n", value1, value2, value1 * value2);
+
+        scanner.close();
     }
 
-    /*public class void main(String[] args){
-        //var value = 5 + 9 * 18 - 5/2;
+    /*
+    public static void main(String[] args) {
+        // var value = 5 + 9 * 18 - 5/2;
         var value = 5;
         value += 12;
-        //value *= /= -=
+        // value *=, /=, -=
         System.out.println(value);
+    }
     */
 }
